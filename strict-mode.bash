c2e958c500ff84ceedb88a470f737ee644f68fb9
Greg Pfeil <greg@technomadic.org>
2022-12-03 01:11:35 -0700

    Conditionalize inherit_errexit on Bash version

--- strict-mode.bash
+++ strict-mode.bash
@@ -2,7 +2,9 @@ set -o errexit \
     -o nounset \
     -o pipefail
 
-shopt -s inherit_errexit
+# `inherit_errexit` was only added in Bash 4.4, and MacOS still includes 3.2.
+[[ "${BASH_VERSINFO[0]}" -ge 4 && "${BASH_VERSINFO[1]}" -ge 4 ]] \
+  && shopt -s inherit_errexit
 
 # shellcheck disable=SC2154 # `s` is assigned, but shellcheck can’t tell.
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
