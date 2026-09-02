b7ea4659101bedd04b4665dd995fc4bc9320eda2
Greg Pfeil <greg@technomadic.org>
2026-09-01 23:28:44 -0600

    Work around `patchShebangs` issue
    It can break when the command to run is a script (as it is here).

--- nix/packages/bash-strict-mode.nix
+++ nix/packages/bash-strict-mode.nix
@@ -85,7 +85,16 @@ shellchecked (strictBuilder "strict-bash" (stdenv.mkDerivation {
     export PATH="$out/bin:$PATH"
     # should find things in `PATH`
     ./test/is-on-path
-    (set +u; patchShebangs ./.cache/test/strict-bash)
+    ## NOTE: This can’t use `patchShebangs`, because `strict-bash` is a script,
+    ##       and (depending on the OS & shell) scripts are rejected as
+    ##       interpreters, with misleading fallback behavior. Instead, we make
+    ##       Bash the interpreter, and have it call the full path of
+    ##       `strict-bash`.
+    for t in ./.cache/test/strict-bash/*; do
+      if [ -x "$t" ]; then
+        sed -i "1s|.*|#!$(command -v bash) $out/bin/strict-bash|" "$t"
+      fi
+    done
     env -u BASH_ENV -u SHELLOPTS -u BASHOPTS \
       bats --print-output-on-failure ./.cache/test/strict-bash/all-tests.bats
     runHook postInstallCheck
