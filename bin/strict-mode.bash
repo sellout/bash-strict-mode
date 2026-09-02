7a1b95561155262413f967f79b9c49f11ec0d3f7
Greg Pfeil <greg@technomadic.org>
2026-09-01 18:33:27 -0600

    Always try `inherit_errexit`
    Rather than correct the version comparison, just _try_ enabling
    `inherit_errexit`, and let it slide if we can’t. Much more robust to shell
    implementation changes.
    
    Fixes #54.

--- bin/strict-mode.bash
+++ bin/strict-mode.bash
@@ -3,9 +3,10 @@ set \
   -o nounset \
   -o pipefail
 
-# `inherit_errexit` was only added in Bash 4.4, and MacOS still includes 3.2.
-[[ ${BASH_VERSINFO[0]} -ge 4 && ${BASH_VERSINFO[1]} -ge 4 ]] \
-  && shopt -s inherit_errexit
+# `inherit_errexit` was only added in Bash 4.4, and MacOS still includes 3.2,
+# so it can’t simply be set unconditionally. Try setting it, and just let it go
+# if the shell doesn’t support it.
+shopt -s inherit_errexit 2> /dev/null || true
 
 # shellcheck disable=SC2154 # `s` is assigned, but shellcheck can’t tell.
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
