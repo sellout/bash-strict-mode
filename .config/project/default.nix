cbb472d57660055899d6b774354dfb11eccb792e
Greg Pfeil <greg@technomadic.org>
2026-09-02 00:21:28 -0600

    Switch Project Manager generation

--- .config/project/default.nix
+++ .config/project/default.nix
@@ -10,18 +10,10 @@
     summary = "Write better shell scripts";
   };
 
-  editorconfig.enable = true;
-
   programs = {
-    direnv.enable = true;
-    # This should default by whether there is a .git file/dir (and whether it’s
-    # a file (worktree) or dir determines other things – like where hooks
-    # are installed.
-    git.enable = true;
     treefmt = let
       shellFiles = ["*.bash" "bin/*" "test/*"];
     in {
-      enable = true;
       programs = {
         ## Shell linter
         shellcheck.enable = true;
@@ -41,37 +33,15 @@
         shfmt.includes = shellFiles;
       };
     };
-    vale = {
-      enable = true;
-      excludes = [
-        "./bin/strict-bash"
-        "./test/generate"
-        "./test/is-on-path"
-      ];
-    };
+    vale.excludes = [
+      "./bin/strict-bash"
+      "./test/generate"
+      "./test/is-on-path"
+    ];
   };
 
-  services = {
-    flakehub.enable = true;
-    garnix.enable = true;
-    github = {
-      enable = true;
-      settings = {
-        ## FIXME: Shouldn’t need `mkForce` here (or to duplicate the base
-        ##        contexts). Need to improve module merging.
-        branches.main.protection.required_status_checks.contexts =
-          lib.mkForce
-          (flaky.lib.forGarnixSystems supportedSystems (sys: [
-            "homeConfig ${sys}-example"
-            "package ${config.project.name} [${sys}]"
-            "package default [${sys}]"
-            ## FIXME: These are duplicated from the base config
-            "check formatter [${sys}]"
-            "devShell default [${sys}]"
-          ]));
-        repository.topics = ["bash" "development" "nix-flakes"];
-      };
-    };
-    renovate.enable = true;
+  services.github.settings.repository = {
+    private = false;
+    topics = ["bash" "development" "nix-flakes"];
   };
 }
